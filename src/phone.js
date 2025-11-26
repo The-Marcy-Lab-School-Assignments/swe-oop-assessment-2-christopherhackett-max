621105ac4323a8b62227af70459e4cdcf8b94a43
christopherhackett-max <christopherhackett20@gmail.com>
2025-11-25 22:01:24 -0500

    Revert "Draft"

--- src/phone.js
+++ src/phone.js
@@ -1,49 +1,13 @@
 class Phone {
-  batteryLevel = 100;
 
-  constructor(brand, model, password) {
-    this.brand = brand;
-    this.model = model;
-    this.password = password;
-  }
-
-  makeCall(number) {
-    this.batteryLevel -= 5;
-    return `Calling ${number}`;
-  }
-
-  charge() {
-    this.batteryLevel = 100;
-    return 'Phone fully charged';
-  }
-
-  unlock(password) {
-    if (password === this.password) {
-      return true;
-    }
-    return false;
-  }
 }
 
-class iPhone extends Phone {
-  batteryLevel = 100;
+class iPhone {
 
-  constructor(model, password, numberOfCameras) {
-    super('Apple', model, password);
-    this.numberOfCameras = numberOfCameras;
-  }
-
-  makeCall(number) {
-    return `${super.makeCall(number)} using FaceTime audio`;
-  }
-
-  faceTime(name) {
-    this.batteryLevel -= 10;
-    return `Facetiming ${name}`;
-  }
 }
 
 // TEST YOUR CODE HERE
+
 
 // DO NOT REMOVE
 module.exports = { Phone, iPhone };
--- src/pokemon.js
+++ src/pokemon.js
@@ -1,49 +1,5 @@
 class Pokemon {
-  static allPokemon = [];
 
-  #health = 100;
-
-  #level = 1;
-
-  constructor(name, type) {
-    this.name = name;
-    this.type = type;
-    Pokemon.allPokemon.push(this);
-  }
-
-  getHealth() {
-    return this.#health;
-  }
-
-  getLevel() {
-    return this.#level;
-  }
-
-  levelUp() {
-    this.#level += 1;
-    this.#health += 10;
-    console.log(`${this.name} leveled up to level ${this.#level}!`);
-  }
-
-  isFainted() {
-    if (this.#health <= 0) {
-      return true;
-    }
-    return false;
-  }
-
-  attack(targetPokemon) {
-    targetPokemon.#health -= (10 * this.#level);
-    console.log(`${this.name} attacked ${targetPokemon.name}!`);
-  }
-
-  static getTotalPokemon() {
-    return Pokemon.allPokemon.length;
-  }
-
-  static findByName(name) {
-    return Pokemon.allPokemon.find((pokemon) => pokemon.name === name);
-  }
 }
 
 // TEST YOUR CODE HERE
